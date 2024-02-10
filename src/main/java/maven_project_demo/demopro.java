2c184ce55ed9b8d492f199bbe5a3fc100291565f
Digvijay <2023.digvijay.deshmukh@ves.ac.in>
2024-02-10 19:51:27 +0530

    Initial Commit

---
+++ src/main/java/maven_project_demo/demopro.java
@@ -0,0 +1,7 @@
+package maven_project_demo;
+
+public class demopro {
+	public String exMethod() {
+		return "exMethod";
+	}
+}
---
+++ src/test/java/maven_project_demo/exMethod.java
@@ -0,0 +1,14 @@
+package maven_project_demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class exMethod {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
